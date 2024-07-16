cf0647a2a73d35bae19359c3ba688ddfcb3e89d5
songa <thddk0121@gmail.com>
2024-07-16 16:37:19 +0900

    feat: 리액트 쿼리 기능 추가
    - QueryClient, QueryClientProvider, ReactQueryDevtools 구현
    - Homepage 컴포넌트를 App에 포함

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import HomePage from './HomePage';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+// QueryClient 인스턴스 생성
+// 애플리케이션의 모든 쿼리와 뮤테이션 관리
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
-      <h1>studylog</h1>
-    </>
+    // 쿼리 클라이언트를 제공하는 QueryClientProvider를 설정
+    // -> 모든 하위 컴포넌트에서 리액트 쿼리 기능 사용 가능
+    <QueryClientProvider client={queryClient}>
+      <HomePage />
+
+      {/* ReactQueryDevtools 추가해 개발 중 쿼리 상태 모니터링과 디버깅 가능 */}
+      {/* 리액트 쿼리 개발자 도구가 처음에는 닫혀 있음 */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/HomePage.jsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div>StudyLog</div>;
+}
+
+export default HomePage;
