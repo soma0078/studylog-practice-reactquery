900991a178b582217d392c1d0177ae018d7c636e
songa <thddk0121@gmail.com>
2024-07-17 23:42:03 +0900

    feat: useQeury에 staleTime옵션 1분 설정

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getPosts } from './api';
 
 function HomePage() {
-  const result = useQuery({ queryKey: ['posts'], queryFn: getPosts });
+  const result = useQuery({ queryKey: ['posts'], queryFn: getPosts, staleTime: 60 * 1000, gcTime: (60 * 1000) % 10 });
   console.log(result);
 
   return <div>StudyLog</div>;
