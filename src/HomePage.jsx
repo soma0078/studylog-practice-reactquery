c6f548c3bc6cb612ab9b97a2a09dfa17bddc4345
songa <thddk0121@gmail.com>
2024-07-17 17:10:03 +0900

    feat: HomePage에 useQuery로 데이터 fetch 기능 추가

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
+import { getPosts } from './api';
+
 function HomePage() {
+  const result = useQuery({ queryKey: ['posts'], queryFn: getPosts });
+  console.log(result);
+
   return <div>StudyLog</div>;
 }
 
